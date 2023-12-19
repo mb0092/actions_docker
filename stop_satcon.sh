f24bc1804027376e30da3d8bbe4483b0ddd0dae4
mb0092 <miroslav.babka@email.cz>
2023-12-19 11:59:24 +0100

    bash

--- stop_satcon.sh
+++ stop_satcon.sh
@@ -1,3 +1,5 @@
+cd /home/miro/docker-tomcat
+echo "Removing old instances"
 docker stop $(docker ps -a | grep satcon | awk '{print substr ($0, 0, 12)}')
 docker rm $(docker ps -a | grep satcon | awk '{print substr ($0, 0, 12)}')
 docker image rm satcon
